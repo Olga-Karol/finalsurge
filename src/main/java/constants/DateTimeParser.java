c672082e669cbc592e60a81680f4820a07118b3b
Olga Karol <volya.karol@gmail.com>
2022-04-12 21:33:34 +0400

    Revert "updates"
    This reverts commit 1f2514155a5b0d331e930c8a6efe9eaf2e3153b8.

--- src/main/java/constants/DateTimeParser.java
+++ src/main/java/constants/DateTimeParser.java
@@ -2,6 +2,6 @@ package constants;
 
 public class DateTimeParser {
 
-    public static final String INPUT_DATETIME_FORMAT = "MMMM d yyyy";
+    public static final String INPUT_DATETIME_FORMAT = "EEEE, MMMM d, yyyy";
     public static final String OUTPUT_DATE_FORMAT = "M/d/yyyy";
 }
